9cbbf0270b591c41db0e8994dc7ab9f279f998cc
itscool2b <itscool2@outlook.com>
2024-01-09 10:39:23 -0500

    yes

--- OfficalSoccerApp/settings.py
+++ OfficalSoccerApp/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') + ['reissoccer.onrender.com']
-
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') + ['reissoccer.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
