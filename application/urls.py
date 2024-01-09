7151c0bba898683057f22cce58060dd521d51cc0
itscool2b <itscool2@outlook.com>
2024-01-09 08:56:16 -0500

    admin

--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import add_game
+from django.contrib import admin
 urlpatterns = [
     path("", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
@@ -9,5 +10,6 @@ urlpatterns = [
     path("add_game/", add_game, name='add_game'),
     path("player/", views.player_create_view, name="createplayer"),
     path("playerhome/", views.playerhome, name='playerhome'),
+    path('admin/', admin.site.urls),
     
 ]
